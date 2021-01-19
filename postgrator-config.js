0cf9fc573ddc0927aaaedafc6328b1cc1f82f856
Anthony Swank <anthonyswank1999@gmail.com>
2021-01-19 15:02:35 -0500

    fixed database stuff

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
+  "connectionString": process.env.NODE_ENV
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL,
   "host": process.env.MIGRATION_DB_HOST || 'localhost',
   "port": process.env.MIGRATION_DB_PORT || '5432',
   "database": process.env.MIGRATION_DB_NAME || 'noteful',
--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT : process.env.PORT || 8000,
   NODE_ENV : process.env.NODE_ENV,
   API_KEY : process.env.API_KEY,
-  DB_URL : process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-api',
-  TEST_DB_URL : process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-api-test'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful-test'
 }
