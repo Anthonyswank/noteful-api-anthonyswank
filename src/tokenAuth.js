fcad3a0b04a2ec1bb621fd839251ea86b4e603d8
Anthony Swank <anthonyswank1999@gmail.com>
2021-01-19 15:47:17 -0500

    fixed authorization

--- src/tokenAuth.js
+++ src/tokenAuth.js
@@ -4,7 +4,7 @@ const logger = require( './logger' )
 
 function tokenAuth( req, res, next ) {
   const clientAPIToken = req.get( 'Authorization' )
-  if ( !clientAPIToken || clientAPIToken.split( ' ' )[1] !== API_KEY ) {
+  if (!clientAPIToken || clientAPIToken.split(' ')[1] !== API_KEY ) {
     logger.error( `Unauthorized request to path: ${req.path}. API TOKEN: ${API_KEY} and AUTH TOKEN: ${clientAPIToken}` )
     return res.status( 401 ).json( { error : 'Unauthorized request' } )
   }
