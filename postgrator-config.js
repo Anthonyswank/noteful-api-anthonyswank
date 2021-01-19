82d5c7ad62dcf2e64cce7ba07dfc70ce98a266ee
Anthony Swank <anthonyswank1999@gmail.com>
2021-01-19 15:18:57 -0500

    updated postgrator config

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,9 +4,5 @@ module.exports = {
   "connectionString": process.env.NODE_ENV
   ? process.env.TEST_DATABASE_URL
   : process.env.DATABASE_URL,
-  "host": process.env.MIGRATION_DB_HOST || 'localhost',
-  "port": process.env.MIGRATION_DB_PORT || '5432',
-  "database": process.env.MIGRATION_DB_NAME || 'noteful',
-  "username": process.env.MIGRATION_DB_USER || 'postgres',
-  "password": process.env.MIGRATION_DB_PASS
+  'ssl': !!process.env.SSL,
 }
