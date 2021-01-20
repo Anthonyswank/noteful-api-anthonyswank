3c1f2de3d504194d226afb6b50d95d87bfe31cc7
Anthony Swank <anthonyswank1999@gmail.com>
2021-01-19 19:19:31 -0500

    fixed config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  PORT : process.env.PORT || 8000,
-  NODE_ENV : process.env.NODE_ENV,
-  API_KEY : process.env.API_KEY,
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV,
+  API_KEY: process.env.API_KEY,
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful-test'
 }
